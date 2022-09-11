5895a478c3f937c42cbf62b46780669fbc1f98c9
zexianghuang <111756902+zexianghuang@users.noreply.github.com>
2022-09-11 14:36:35 +0800

    Update dwd_ethereum_trans_info_di.sql

--- ethereum_dwd/dwd_ethereum_trans_info_di.sql
+++ ethereum_dwd/dwd_ethereum_trans_info_di.sql
@@ -1,1 +1,4 @@
-select
+insert into tagee.tagee_dwd.dwd_ethereum_trans_info_di(transHash ,nonce,transactionIndex,fromAddress,toAddress,transValue,gas,gasPrice,input,maxfeePerGas,maxpriorityfeePerGas,transactionType,blockHash,blockNumber,blockMiner,blockDifficulty,blockTotalDifficulty ,blockSize,blockgasLimit,blockgasUsed ,blocktimestamp,fromAdress,fromBalance,toAdress,toBalance)
+select 
+
+from 
