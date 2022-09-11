4f5c3ba0f6ceeb306f98ddaba3ca5ea34966553b
zexianghuang <111756902+zexianghuang@users.noreply.github.com>
2022-09-11 14:14:43 +0800

    Update dwd_ethereum_block_info_da.sql

--- ethereum_dwd/dwd_ethereum_block_info_da.sql
+++ ethereum_dwd/dwd_ethereum_block_info_da.sql
@@ -1,5 +1,5 @@
 insert into tagee.tagee_dwd.dwd_ethereum_block_info_da(blockNumber,blockHash,parentHash,nonce,sha3Uncles,logsBloom,transactionsRoot,stateRoot,receiptsRoot,miner,difficulty,totalDifficulty,blockSize,extraData,gasLimit,gasUsed,blocktimestamp,transactionCount,baseFeePerGas,reward)
-select 
+select
        blockNumber               -- 区块号。当这个区块处于pending将会返回null。
       ,blockHash                 -- 区块的哈希串。当这个区块处于pending将会返回null。
       ,parentHash                -- 32字节的父区块的哈希值。
@@ -13,11 +13,11 @@ select
       ,difficulty                -- 当前块的难度，整数。
       ,totalDifficulty           -- 区块链到当前块的总难度，整数。
       ,blockSize                 -- 当前这个块的字节大小。
-      ,extraData                 -- 当前块的extra data字段。 
+      ,extraData                 -- 当前块的extra data字段。
       ,gasLimit                  -- 当前区块允许使用的最大gas。
       ,gasUsed                   -- 当前区块累计使用的总的gas。
       ,blocktimestamp            -- 区块打包时的unix时间戳。
       ,transactionCount          -- 区块交易总数
       ,baseFeePerGas             -- 交易费用基础gas
-      ,(gasUsed * baseFeePerGas) reward  -- 矿工奖励 
-from tagee.tagee_ods.ods_ethereum_bolck_info_da
+      ,(gasUsed * baseFeePerGas) reward  -- 矿工奖励
+from tagee.tagee_ods.ods_ethereum_block_info_da
